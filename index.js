7f846a51fdd6c0d25a2be1c7af29439e2660a4fb
Scott <sdmccoy@gmail.com>
2017-09-21 12:33:01 -0700

    adjusted webpack paths

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 //static page that express routes to. Webpack builds the new html in this dir
-app.use(express.static(`${__dirname}/build`));
+app.use(express.static(`${__dirname}/docs`));
 
-app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/docs/index.html`));
 app.listen(process.env.PORT, () => {
   console.log('Server running on PORT ', process.env.PORT);
 });
