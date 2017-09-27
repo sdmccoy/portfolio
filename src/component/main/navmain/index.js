11c6315d21021b141b8d24da88eb2dc8c8801725
Scott <sdmccoy@gmail.com>
2017-09-26 20:28:53 -0700

    basic style for main nav buttons

--- src/component/main/navmain/index.js
+++ src/component/main/navmain/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './_navmain.scss';
 
 class NavMain extends React.Component {
 
   render(){
     return(
       <div className='navmain'>
-        <Link to='/projects' className='projects-icon'>Projects</Link>
-        <Link to='/community' className='community-icon'>Community</Link>
-        <Link to='/about' className='about-icon'>About</Link>
+        <Link to='/projects' className='projects-icon'><p>Projects</p></Link>
+        <Link to='/community' className='community-icon'><p>Community</p></Link>
+        <Link to='/about' className='about-icon'><p>About</p></Link>
       </div>
     );
   }
