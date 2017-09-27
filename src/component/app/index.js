991b7cae525631f5bfb39303d81416e5fddb757d
Scott <sdmccoy@gmail.com>
2017-09-27 12:34:10 -0700

    hello world is up for projects route. Header and footer will persist and the main will change based on route

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import Dashboard from '../dashboard';
+import NavBar from '../header/navbar';
+import Hero from '../header/hero';
+import ContactForm from '../main/contact-form';
+import NavMain from '../main/navmain';
+import Footer from '../footer';
+import Projects from '../main/projects';
+
 import '../../style/_main.scss';
 
 class App extends React.Component {
 
   render(){
     return(
-      <div className='app'>
-        <BrowserRouter>
-          <div className='route-container'>
-            <Route exact path='/' component={Dashboard} />
+      <BrowserRouter>
+        <div className='app'>
+          <div className='header'>
+            <NavBar />
+            <Hero />
           </div>
-        </BrowserRouter>
-      </div>
+          <div className='main'>
+            <Route exact path='/' component={NavMain} />
+            <Route exact path='/projects' component={Projects} />
+          </div>
+          <div className='footer'>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+
+// <ContactForm />
+class Dashboard extends React.Component {
+  render(){
+    return(
+      <div className='dashboard-container'>
+      </div>
+    );
+  }
+}
---
+++ src/component/main/projects/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Projects extends React.Component {
+
+  render(){
+    return(
+      <div className='projects'>
+        Hello world projects.
+      </div>
+    );
+  }
+}
+
+export default Projects;
