c2c0de2294ccea7a3f2d28b4100727f3ff336ad3
Scott <sdmccoy@gmail.com>
2017-09-28 10:37:55 -0700

    integrated media query dep for nav bar conditionals based on screen size

--- src/component/header/navbar/index.js
+++ src/component/header/navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 import * as util from '../../../lib/util.js';
 import './_navbar.scss';
 
@@ -7,37 +8,42 @@ class NavBar extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      showNavMenu: false,
+      showHamburgerMenu: false,
     };
     this.handleCompassClick = this.handleCompassClick.bind(this);
   }
 
   handleCompassClick(){
-    !this.state.showNavMenu ?
-      this.setState({showNavMenu: true}) :
-      this.setState({showNavMenu: false});
+    !this.state.showHamburgerMenu ?
+      this.setState({showHamburgerMenu: true}) :
+      this.setState({showHamburgerMenu: false});
   }
 
   render(){
     return(
       <nav className='navbar'>
-        <i onClick={this.handleCompassClick} className='fa fa-compass' aria-hidden='true'></i>
+        <MediaQuery minWidth={760}>
+          <Link to='/'><i className="fa fa-home" aria-hidden="true"></i></Link>
+          <Link to='/projects'>Projects</Link>
+          <Link to='/community'>Community</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </MediaQuery>
+        <MediaQuery maxWidth={759}>
+          <i onClick={this.handleCompassClick} className='fa fa-compass' aria-hidden='true'></i>
+        </MediaQuery>
         <Link to='https://www.linkedin.com/in/sdmccoy/' target='_blank'>
           <i className='fa fa-linkedin-square' aria-hidden='true'></i>
         </Link>
         <Link to='https://github.com/sdmccoy' target='_blank'>
           <i className='fa fa-github-square' aria-hidden='true'></i>
         </Link>
-        {util.renderIf(this.state.showNavMenu,
+        {util.renderIf(this.state.showHamburgerMenu,
           <div className='navbar-menu'>
             <Link to='/'>Home</Link>
-            <div className='clear-float'></div>
             <Link to='/projects'>Projects</Link>
-            <div className='clear-float'></div>
             <Link to='/community'>Community</Link>
-            <div className='clear-float'></div>
             <Link to='/about'>About</Link>
-            <div className='clear-float'></div>
             <Link to='/contact'>Contact</Link>
           </div>
         )}
