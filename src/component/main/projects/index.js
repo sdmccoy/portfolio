6b6fe2d0d7f0235a0a5bc862af795ee4af2ba783
Scott <sdmccoy@gmail.com>
2017-09-29 16:26:03 -0700

    headline, title, image, and brief are styled in mobile view

--- src/component/main/projects/index.js
+++ src/component/main/projects/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as util from '../../../lib/util.js';
 import * as devIcon from '../../../lib/dev-icons.js';
+import './_projects.scss';
 //remove after creating object and icon vars
 import sparkleMonkeyPhoto from '../../../../assets/project-photo-sparklemonkey-medium.png';
 
@@ -25,25 +26,25 @@ class Projects extends React.Component {
 
     return(
       <div className='projects'>
-        <h2 className='projects-headline'>
+        <h2 className='headline'>
           Projects
         </h2>
-        <div className='project-photo'>
+        <div className='device-photo'>
           <img src={sparkleMonkeyPhoto} />
         </div>
         <div className='snapshot'>
           <h2 className='title'>Sparkle Monkey</h2>
-          <h6 className='brief'>Brief</h6>
+          <h6 className='brief'>Brief :</h6>
           <p>Spicy jalapeno bacon ipsum dolor amet shankle chuck filet mignon drumstick ground round chicken pig pork tail cow salami porchetta boudin prosciutto jowl. Sausage ham hock tongue ribeye, turducken rump pancetta hamburger alcatra pork chop salami. Meatball boudin biltong ground round frankfurter landjaeger pig. Chicken pork loin chuck, boudin salami beef ribs short ribs venison brisket pork chop ham. Tail capicola tri-tip pork pork chop, pork belly alcatra tongue ground round kielbasa fatback shank bacon kevin. Shankle pastrami tri-tip corned beef pork loin pork belly doner leberkas prosciutto capicola cow jerky.</p>
         </div>
         <div className='button-bar'>
-          <button onClick={this.handleProjectDetails}>
-            <i className="fa fa-chevron-circle-down" aria-hidden="true"></i>
-            <h6>Details</h6>
-          </button>
           <button>
             <h6>Visit Site</h6>
             <i className="fa fa-arrow-circle-right" aria-hidden="true"></i>
+          </button>
+          <button onClick={this.handleProjectDetails}>
+            <h6>Details</h6>
+            <i className="fa fa-chevron-circle-down" aria-hidden="true"></i>
           </button>
         </div>
         {util.renderIf(this.state.showProjectDetails,
