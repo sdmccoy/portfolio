744bada7b73aafa45617aeecface2bc75b1107ca
Scott <sdmccoy@gmail.com>
2017-09-29 13:54:14 -0700

    mockup for project page

--- src/component/main/projects/index.js
+++ src/component/main/projects/index.js
@@ -1,11 +1,78 @@
 import React from 'react';
+import * as util from '../../../lib/util.js';
+
+//remove after creating object and icon vars
+import sparkleMonkeyPhoto from '../../../../assets/project-photo-sparklemonkey-medium.png';
+import JSIcon from '../../../../assets/devicons/javascript-original.svg';
+import nodeIcon from '../../../../assets/devicons/nodejs-original-wordmark.svg';
+import reactIcon from '../../../../assets/devicons/react-original-wordmark.svg';
+import jQueryIcon from '../../../../assets/devicons/jquery-original-wordmark.svg';
+import HTML5Icon from '../../../../assets/devicons/html5-original-wordmark.svg';
+import CSS3Icon from '../../../../assets/devicons/css3-original-wordmark.svg';
+import sassIcon from '../../../../assets/devicons/sass-original.svg';
+import postgreSQLIcon from '../../../../assets/devicons/postgresql-original-wordmark.svg';
+import mongoDBIcon from '../../../../assets/devicons/mongodb-original-wordmark.svg';
+import webpackIcon from '../../../../assets/devicons/webpack-original-wordmark.svg';
 
 class Projects extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showProjectDetails: false,
+    };
+    this.handleProjectDetails = this.handleProjectDetails.bind(this);
+  }
 
+  handleProjectDetails(){
+
+  }
+  //build static structure to style, then make object module loop through
   render(){
     return(
       <div className='projects'>
-        Hello world projects.
+        <h2 className='projects-headline'>
+          Projects
+        </h2>
+        <div className='project-photo'>
+          <img src={sparkleMonkeyPhoto} />
+        </div>
+        <div className='snapshot'>
+          <h2 className='title'>Sparkle Monkey</h2>
+          <h6 className='brief'>Brief</h6>
+          <p>Spicy jalapeno bacon ipsum dolor amet shankle chuck filet mignon drumstick ground round chicken pig pork tail cow salami porchetta boudin prosciutto jowl. Sausage ham hock tongue ribeye, turducken rump pancetta hamburger alcatra pork chop salami. Meatball boudin biltong ground round frankfurter landjaeger pig. Chicken pork loin chuck, boudin salami beef ribs short ribs venison brisket pork chop ham. Tail capicola tri-tip pork pork chop, pork belly alcatra tongue ground round kielbasa fatback shank bacon kevin. Shankle pastrami tri-tip corned beef pork loin pork belly doner leberkas prosciutto capicola cow jerky.</p>
+        </div>
+        <div className='button-bar'>
+          <button onClick={this.handleProjectDetails}>
+            <i className="fa fa-chevron-circle-down" aria-hidden="true"></i>
+            <h6>Details</h6>
+          </button>
+          <button>
+            <h6>Visit Site</h6>
+            <i className="fa fa-arrow-circle-right" aria-hidden="true"></i>
+          </button>
+        </div>
+        {util.renderIf(this.state.showProjectDetails,
+          <div className='details'>
+            <ul className='feature-list'>
+              <li>feature one</li>
+              <li>feature two</li>
+              <li>feature three</li>
+            </ul>
+            <i className="fa fa-times-circle" aria-hidden="true"></i>
+            <div className='devicon-container'>
+              <img src={JSIcon} />
+              <img src={nodeIcon} />
+              <img src={reactIcon} />
+              <img src={jQueryIcon} />
+              <img src={HTML5Icon} />
+              <img src={CSS3Icon} />
+              <img src={sassIcon} />
+              <img src={postgreSQLIcon} />
+              <img src={mongoDBIcon} />
+              <img src={webpackIcon} />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
