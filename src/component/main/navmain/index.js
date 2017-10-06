3e28211e728da28849427c4b19cbe8ac27171a85
Scott <sdmccoy@gmail.com>
2017-10-05 17:00:19 -0700

    nav main is basic styled with

--- src/component/main/navmain/index.js
+++ src/component/main/navmain/index.js
@@ -7,9 +7,18 @@ class NavMain extends React.Component {
   render(){
     return(
       <div className='navmain'>
-        <Link to='/projects' className='projects-icon'><p>Projects</p></Link>
-        <Link to='/community' className='community-icon'><p>Community</p></Link>
-        <Link to='/about' className='about-icon'><p>About</p></Link>
+        <Link to='/projects' className='projects-nav'>
+          <i className="fa fa-laptop" aria-hidden="true"></i>
+          <h4>PROJECTS</h4>
+        </Link>
+        <Link to='/community' className='community-nav'>
+          <i className="fa fa-heartbeat" aria-hidden="true"></i>
+          <h4>COMMUNITY</h4>
+        </Link>
+        <Link to='/about' className='about-nav'>
+          <i className="fa fa-user-circle" aria-hidden="true"></i>
+          <h4>ABOUT</h4>
+        </Link>
       </div>
     );
   }
