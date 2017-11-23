a586231c7e27fe7f72dee8cf4e8359d6ad71b223
Scott <sdmccoy@gmail.com>
2017-11-23 10:49:38 -0800

    building about me

--- src/component/main/about/index.js
+++ src/component/main/about/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import comingSoon from '../../../../assets/coming-soon.png';
 import './_about.scss';
 
@@ -10,8 +11,15 @@ class About extends React.Component {
         <h2 className='headline'>
           ABOUT
         </h2>
-        <h3>Let's go out for <i className="fa fa-coffee" aria-hidden="true"></i> and see what makes each other tick. Otherwise, page is</h3>
-        <img src={comingSoon} />
+        <h3>Let's go out for <i className="fa fa-coffee" aria-hidden="true"></i> and learn more about each other. </h3>
+        <h3>Message me on <Link to='https://www.linkedin.com/in/sdmccoy/'       className='nav-icon' target='_blank'>
+          <i className='fa fa-linkedin-square' aria-hidden='true'></i>
+        </Link> to set it up!</h3>
+        <h3>Here's a little taste of what makes me happy.</h3>
+        <h2>Artificer</h2>
+        <h5>Using that logic-based creativity to hand craft functioning art for others.</h5>
+        <h2>Adventurer</h2>
+        <h5>Harnessing the power of nature to connect on a thrilling level.</h5>
       </div>
     );
   }
